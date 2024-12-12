139a074274ad0a15650b155df380b979b3e041c1
kolihitesh754 <kolihitesh754@gmail.com>
2024-12-12 14:46:47 -0600

    Homepage test

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
+    path("admin/", views.home_page),
     path("admin/", admin.site.urls),
 ]
